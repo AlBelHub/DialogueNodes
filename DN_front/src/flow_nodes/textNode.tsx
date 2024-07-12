7de12fe19cc6c6df611bcfc3012db6dd5562c125
AlBelHub <aleksey.belov.work@gmail.com>
2024-07-12 09:51:02 +0300

    custom node test

---
+++ DN_front/src/flow_nodes/textNode.tsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react';
+
+import {Handle, Position} from 'reactflow';
+
+
+const handleStyle = { left: 10 };
+ 
+function TextUpdaterNode({ data }) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
+ 
+  return (
+    <>
+      <Handle type="target" position={Position.Left} />
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input id="text" name="text" onChange={onChange} className="nodrag" />
+      </div>
+      <Handle type="source" position={Position.Right} id="a" />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="b"
+        style={handleStyle}
+      />
+    </>
+  );
+}
+
+export default TextUpdaterNode;
