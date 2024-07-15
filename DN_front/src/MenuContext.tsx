045abd2bf843a43c62b22e79b01e02a47e879578
AlBelHub <aleksey.belov.work@gmail.com>
2024-07-15 18:35:08 +0300

    Собственное контекстное меню

--- DN_front/src/MenuContext.tsx
+++ DN_front/src/MenuContext.tsx
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {data} from './ContextData/data';
 
-const MenuContext = (data) => {
+import { Node, Position, useReactFlow } from 'reactflow';
+
+
+
+import useStore from './store';
+import { customAlphabet, nanoid } from 'nanoid';
+
+type importedProps = {
+    menu: Boolean,
+    cursorData: Array<number>
+}
+
+//TODO: SCSS LOOP VARS FOR MARGIN FROM CONTEXT MENU
+
+export default function MenuContext({menu, cursorData}: importedProps) 
+{
+    const nanoid = customAlphabet('123456789', 5);
+
+    const { screenToFlowPosition } = useReactFlow();
+
+    const nodes = useStore((state) => state.nodes);
+    const setNodes = useStore((state) => state.setNodes);
+
+    const position = screenToFlowPosition({
+      x: cursorData[0],
+      y: cursorData[1],
+    });
+
+    const id = nanoid();
+
+    const newNode: Node = 
+      {
+        id: id,
+        targetPosition: Position.Left,
+        sourcePosition: Position.Right,
+        data: { label: id },
+        position: {x: position.x, y: position.y-40},
+      };
+
     return(
         <>
-        {data.map((item) => {
-            <div className="menu"
-                onContextMenu={(e) => {
-                    e.preventDefault();
-                    console.log(item);
+        {menu && (
+        <div className='context-menu' style={{left: cursorData[0] + "px", top: cursorData[1] + "px"}}>
+          {
+            data.map((item) => (
+              <div key={item.id} className="context-menu_element noselect"
+                onClick={e => {
+                  setNodes(nodes.concat(newNode));
+                  console.log(position);
                 }}
-            >
-
-            </div>
-        })}
+              >
+                {item.title}
+              </div>
+            ))
+          }
+        </div>
+      )}
         </>
     );
 }
-
-
-export default MenuContext;
